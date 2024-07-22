13a1ff07b4b005f0fb460e9db04deac0c6a95f33
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-07-22 07:30:05 +0300

    feat(header): add base header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,31 @@
-import { FC } from "react"
+import { FC, useContext } from "react"
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react"
+import { ThemeContext } from "../theme-provider"
+import { FaRegMoon } from "react-icons/fa"
+import { LuSunMedium } from "react-icons/lu"
 
 export const Header: FC = () => {
-  return <header className={`header`}>Header</header>
+  const { theme, toggleTheme } = useContext(ThemeContext)
+
+  return (
+    <Navbar className={`bg-background text-foreground ${theme}`}>
+      <NavbarBrand>
+        <p className={`font-bold text-inherit`}>Network Social</p>
+      </NavbarBrand>
+
+      <NavbarContent justify="end">
+        <NavbarItem
+          className={`lg:flex text3xl cursor-pointer`}
+          onClick={() => toggleTheme()}
+        >
+          {theme === "light" ? <FaRegMoon /> : <LuSunMedium />}
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
 }
