91fc999bb27242c84bf45d1f636f08f75c60c447
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-07-25 21:51:22 +0300

    chore(base_url): update Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,24 @@
-import React, { FC, InputHTMLAttributes, ReactElement } from "react"
+import React, { ReactElement } from "react"
 import { Control, useController } from "react-hook-form"
 import { Input as NextInput } from "@nextui-org/react"
 
-interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+type Props = {
   name: string
-  control: Control<any>
   label: string
+  placeholder?: string
+  type?: string
+  control: Control<any>
+  required?: string
   endContent?: ReactElement
 }
 
-export const Input: FC<IInputProps> = ({
+export const Input: React.FC<Props> = ({
   name,
   label,
   placeholder,
   type,
   control,
-  required,
+  required = "",
   endContent,
 }) => {
   const {
