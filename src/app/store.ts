b1263f57e4c93c2c6fc78d5b71800458b6d9b558
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-07-28 16:41:47 +0300

    feat(middleware): connect auth middleware to store znd add comments

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,37 @@
-import type { Action, ThunkAction } from "@reduxjs/toolkit"
-import { configureStore } from "@reduxjs/toolkit"
-import { api } from "../services/api"
-import user from "../feature/userSlice"
+// Этот файл настраивает и конфигурирует хранилище Redux для приложения, включая подключение редюсеров и миддлваров.
+// Он определяет базовые компоненты, необходимые для управления глобальным состоянием, обработки API-запросов
+// и управления сессиями пользователей через миддлвары и типы данных.
 
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit" // Настройка и создание хранилища Redux
+import { api } from "../services/api" // RTK Query API для работы с запросами
+import user from "../feature/userSlice" // Редюсер для управления состоянием пользователя
+import { listenerMiddleware } from "../middleware/auth" // Миддлвар для прослушивания событий
+
+// Создание и конфигурация хранилища Redux
 export const store = configureStore({
   reducer: {
-    [api.reducerPath]: api.reducer,
-    user,
+    // Добавление редюсера, связанного с RTK Query API
+    [api.reducerPath]: api.reducer, // Редюсер для обработки запросов и кэширования данных
+    // Редюсер для управления состоянием пользователя
+    user, // Редюсер для управления данными о пользователе и аутентификацией
   },
+  // Настройка миддлваров
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(api.middleware),
+    getDefaultMiddleware()
+      // Подключение миддлвара для работы с RTK Query API
+      .concat(api.middleware) // Миддлвар для автоматической обработки API-запросов
+      // Добавление кастомного listenerMiddleware для обработки событий
+      .prepend(listenerMiddleware.middleware), // Миддлвар для прослушивания и обработки событий, таких как логин
 })
 
-export type AppStore = typeof store
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = AppStore["dispatch"]
+// Определение типов для хранилища, состояния и диспетчера
+export type AppStore = typeof store // Тип для хранилища Redux
+export type RootState = ReturnType<typeof store.getState> // Тип для состояния приложения
+export type AppDispatch = AppStore["dispatch"] // Тип для функции dispatch
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
   RootState,
   unknown,
   Action
->
+> // Тип для thunk-действий, позволяющий создавать асинхронные действия
