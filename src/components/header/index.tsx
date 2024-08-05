a077deb5433ec609d6a5cb63c0ae600e553cdba4
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-08-05 22:40:11 +0300

    feat(header): add logout func

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import { FC, useContext } from "react"
 import {
+  Button,
   Navbar,
   NavbarBrand,
   NavbarContent,
@@ -8,9 +9,23 @@ import {
 import { ThemeContext } from "../theme-provider"
 import { FaRegMoon } from "react-icons/fa"
 import { LuSunMedium } from "react-icons/lu"
+import { useDispatch, useSelector } from "react-redux"
+import { selectIsAuthenticated } from "../../services/userApi"
+import { useNavigate } from "react-router-dom"
+import { logout } from "../../feature/userSlice"
+import { CiLogout } from "react-icons/ci"
 
 export const Header: FC = () => {
   const { theme, toggleTheme } = useContext(ThemeContext)
+  const isAuthenticated = useSelector(selectIsAuthenticated)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    dispatch(logout())
+    localStorage.removeItem("token")
+    navigate("/auth")
+  }
 
   return (
     <Navbar className={`bg-background text-foreground ${theme}`}>
@@ -26,6 +41,18 @@ export const Header: FC = () => {
           onKeyDown={e => e.key === "Enter" && toggleTheme()}
         >
           {theme === "light" ? <FaRegMoon /> : <LuSunMedium />}
+        </NavbarItem>
+        <NavbarItem>
+          {isAuthenticated && (
+            <Button
+              color="default"
+              variant="flat"
+              className="gap-2"
+              onClick={handleLogout}
+            >
+              <CiLogout /> <span>Выйти</span>
+            </Button>
+          )}
         </NavbarItem>
       </NavbarContent>
     </Navbar>
