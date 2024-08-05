624a78e7ecdb2180641b8da82c324ca8bc56ba43
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-08-05 22:50:34 +0300

    feat(auth): add theme provider

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,34 +1,38 @@
-import { FC, useState } from "react"
+import { FC, useContext, useState } from "react"
 import { Card, CardBody, Tab, Tabs } from "@nextui-org/react"
 import { Login } from "../../feature/login"
 import { Register } from "../../feature/register"
+import { ThemeContext } from "../../components/theme-provider"
 
 export interface IAuthProps {}
 
 export const Auth: FC<IAuthProps> = () => {
   const [selected, setSelected] = useState("login")
+  const { theme } = useContext(ThemeContext)
 
   return (
-    <div className={`flex items-center justify-center h-screen`}>
-      <div className={`flex-col`}>
-        <Card className={`max-w-full w-[340px] h-[450px]`}>
-          <CardBody className={`overflow-hidden`}>
-            <Tabs
-              fullWidth
-              size={"md"}
-              selectedKey={selected}
-              onSelectionChange={key => setSelected(key as string)}
-            >
-              <Tab key={"login"} title={`Вход`}>
-                <Login setSelected={setSelected} />
-              </Tab>
-              <Tab key={"register"} title={`Регистрация`}>
-                <Register setSelected={setSelected} />
-              </Tab>
-            </Tabs>
-          </CardBody>
-        </Card>
+    <main className={`${theme} min-h-screen text-foreground bg-background`}>
+      <div className={`${theme} flex items-center justify-center h-screen`}>
+        <div className={`flex-col`}>
+          <Card className={`max-w-full w-[340px] h-[450px]`}>
+            <CardBody className={`overflow-hidden`}>
+              <Tabs
+                fullWidth
+                size={"md"}
+                selectedKey={selected}
+                onSelectionChange={key => setSelected(key as string)}
+              >
+                <Tab key={"login"} title={`Вход`}>
+                  <Login setSelected={setSelected} />
+                </Tab>
+                <Tab key={"register"} title={`Регистрация`}>
+                  <Register setSelected={setSelected} />
+                </Tab>
+              </Tabs>
+            </CardBody>
+          </Card>
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
