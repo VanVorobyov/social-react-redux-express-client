85cae500bb97edb299d8fd6d247943d643f6f154
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-08-07 22:22:38 +0300

    feat(posts): add create post component

---
+++ src/components/create-post/index.tsx
@@ -0,0 +1,59 @@
+import { Button, Textarea } from "@nextui-org/react"
+import { IoMdCreate } from "react-icons/io"
+import {
+  useCreatePostMutation,
+  useLazyGetAllPostsQuery,
+} from "../../services/postApi"
+import { Controller, useForm } from "react-hook-form"
+
+export const CreatePost = () => {
+  const [createPost] = useCreatePostMutation()
+  const [triggerGetAllPosts] = useLazyGetAllPostsQuery()
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+  } = useForm()
+
+  const onSubmit = handleSubmit(async data => {
+    try {
+      await createPost({ content: data.post }).unwrap()
+      setValue("post", "")
+      await triggerGetAllPosts().unwrap()
+    } catch (error) {
+      console.log("err", error)
+    }
+  })
+  const error = errors?.post?.message as string
+
+  return (
+    <form className="flex-grow" onSubmit={onSubmit}>
+      <Controller
+        name="post"
+        control={control}
+        defaultValue=""
+        rules={{
+          required: "Обязательное поле",
+        }}
+        render={({ field }) => (
+          <Textarea
+            {...field}
+            labelPlacement="outside"
+            placeholder="О чем думайте?"
+            className="mb-5"
+          />
+        )}
+      />
+      {error && <p className="text-red-500 mt-2 mb-5 text-small">{error}</p>}
+      <Button
+        color="success"
+        className="flex-end"
+        endContent={<IoMdCreate />}
+        type="submit"
+      >
+        Добавить пост
+      </Button>
+    </form>
+  )
+}
