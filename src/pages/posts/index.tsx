3e619fd6880ac4304b9ce2274dc3e3e83edbf845
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-08-07 22:23:03 +0300

    feat(posts): add basic post's page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,18 @@
 import { FC } from "react"
+import { useGetAllPostsQuery } from "../../services/postApi"
+import { CreatePost } from "../../components/create-post"
 
 export interface IPostsProps {}
 
 export const Posts: FC<IPostsProps> = () => {
-  return <div className={`posts`}>Posts</div>
+  const { data } = useGetAllPostsQuery()
+
+  return (
+    <>
+      <div className="mb-10 w-full flex">
+        <CreatePost />
+      </div>
+      {data && data.length > 0 ? data.map(() => <div>Карточка</div>) : null}
+    </>
+  )
 }
