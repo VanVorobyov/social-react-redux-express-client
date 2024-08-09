be68eabe6b452ba25e25c575863ae808da587397
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-08-09 23:14:32 +0300

    fix(card): add base card with delete button

---
+++ src/components/card/index.tsx
@@ -0,0 +1,93 @@
+import { FC, useState } from "react"
+
+import { Card as NextUiCard, CardHeader, Spinner } from "@nextui-org/react"
+import {
+  useDislikePostMutation,
+  useLikePostMutation,
+} from "../../services/likeApi"
+import {
+  useDeletePostMutation,
+  useLazyGetAllPostsQuery,
+  useLazyGetPostByIdQuery,
+} from "../../services/postApi"
+import { useDeleteCommentMutation } from "../../services/commentApi"
+import { Link, useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { selectCurrent } from "../../services/userApi"
+import { User } from "../user"
+import { formatToClientDate } from "../../utils/format-to-client-date"
+import { RiDeleteBinLine } from "react-icons/ri"
+import { hasErrorField } from "../../utils/has-error-field"
+
+type TCardProps = {
+  avatarUrl: string
+  name: string
+  authorId: string
+  content: string
+  commentId?: string
+  likesCount?: number
+  commentsCount?: number
+  createdAt?: Date
+  id?: string
+  cardFor: "comment" | "post" | "current-post"
+  likedByUser?: boolean
+}
+
+export const Card: FC<TCardProps> = ({
+  avatarUrl = "",
+  name = "",
+  authorId = "",
+  content = "",
+  commentId = "",
+  likesCount = 0,
+  commentsCount = 0,
+  createdAt,
+  id = "",
+  cardFor = "post",
+  likedByUser = false,
+}) => {
+  const [likePost] = useLikePostMutation()
+  const [dislikePost] = useDislikePostMutation()
+  const [triggerGetAllPosts] = useLazyGetAllPostsQuery()
+  const [triggerGetPostById] = useLazyGetPostByIdQuery()
+  const [deletePost, deletePostStatus] = useDeletePostMutation()
+  const [deleteComment, deleteCommentStatus] = useDeleteCommentMutation()
+  const currentUser = useSelector(selectCurrent)
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+
+  const handleDeletePost = async () => {
+    try {
+      await deletePost(id).unwrap()
+      await triggerGetAllPosts().unwrap()
+    } catch (err) {
+      if (hasErrorField(err)) {
+        setError(err.data.error)
+      }
+    }
+  }
+
+  return (
+    <NextUiCard className="mb-5">
+      <CardHeader className="justify-between items-center bg-transparent">
+        <Link to={`/users/${authorId}`}>
+          <User
+            name={name}
+            className="text-small font-semibold leading-none text-default-600"
+            avatarUrl={avatarUrl}
+            description={createdAt && formatToClientDate(createdAt)}
+          />
+        </Link>
+        {authorId === currentUser?.id && (
+          <div className="cursor-pointer" onClick={handleDeletePost}>
+            {deletePostStatus.isLoading || deleteCommentStatus.isLoading ? (
+              <Spinner />
+            ) : (
+              <RiDeleteBinLine />
+            )}
+          </div>
+        )}
+      </CardHeader>
+    </NextUiCard>
+  )
+}
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,8 @@
-import { FC } from "react"
-import { useGetAllPostsQuery } from "../../services/postApi"
+import { Card } from "../../components/card"
 import { CreatePost } from "../../components/create-post"
+import { useGetAllPostsQuery } from "../../services/postApi"
 
-export interface IPostsProps {}
-
-export const Posts: FC<IPostsProps> = () => {
+export const Posts = () => {
   const { data } = useGetAllPostsQuery()
 
   return (
@@ -12,7 +10,34 @@ export const Posts: FC<IPostsProps> = () => {
       <div className="mb-10 w-full flex">
         <CreatePost />
       </div>
-      {data && data.length > 0 ? data.map(() => <div>Карточка</div>) : null}
+      {data && data.length > 0
+        ? data.map(
+            ({
+              content,
+              author,
+              id,
+              authorId,
+              comments,
+              likes,
+              likedByUser,
+              createdAt,
+            }) => (
+              <Card
+                key={id}
+                avatarUrl={author.avatarUrl ?? ""}
+                content={content}
+                name={author.name ?? ""}
+                likesCount={likes.length}
+                commentsCount={comments.length}
+                authorId={authorId}
+                id={id}
+                likedByUser={likedByUser}
+                createdAt={createdAt}
+                cardFor="post"
+              />
+            ),
+          )
+        : null}
     </>
   )
 }
