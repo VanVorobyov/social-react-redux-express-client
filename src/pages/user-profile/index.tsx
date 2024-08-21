2944e9f0a4cea7bd2a1eb893481af3e63de686cb
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-08-21 23:22:12 +0300

    feat(profile): add profile-info component

--- src/pages/user-profile/index.tsx
+++ src/pages/user-profile/index.tsx
@@ -1,7 +1,136 @@
-import { FC } from "react"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import {
+  selectCurrent,
+  useGetUserByIdQuery,
+  useLazyCurrentQuery,
+  useLazyGetUserByIdQuery,
+} from "../../services/userApi"
+import { useDispatch, useSelector } from "react-redux"
+import { Button, Card, Image, useDisclosure } from "@nextui-org/react"
+import {
+  MdOutlinePersonAddAlt1,
+  MdOutlinePersonAddDisabled,
+} from "react-icons/md"
+import { GoBack } from "../../components/go-back"
+import { BASE_URL } from "../../utils/constants"
+import { CiEdit } from "react-icons/ci"
+import { formatToClientDate } from "../../utils/format-to-client-date"
+import { resetUser } from "../../feature/userSlice"
+import { EditProfile } from "../../components/edit-profile"
+import { ProfileInfo } from "../../components/profile-info"
+import { CountInfo } from "../../components/count-info"
+import {
+  useFollowUserMutation,
+  useUnfollowUserMutation,
+} from "../../services/followApi"
 
-export interface IUserProfileProps {}
+export const UserProfile = () => {
+  const { id } = useParams<{ id: string }>()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const currentUser = useSelector(selectCurrent)
+  const { data } = useGetUserByIdQuery(id ?? "")
+  const [followUser] = useFollowUserMutation()
+  const [unfolowUser] = useUnfollowUserMutation()
+  const [triggerGetUserByIdQuery] = useLazyGetUserByIdQuery()
+  const [triggerCurrentQuery] = useLazyCurrentQuery()
 
-export const UserProfile: FC<IUserProfileProps> = () => {
-  return <div className={`user-profile`}>UserProfile</div>
+  const dispatch = useDispatch()
+
+  useEffect(
+    () => () => {
+      dispatch(resetUser())
+    },
+    [],
+  )
+
+  console.log(`test --> `, test)
+
+  const handleFollow = async () => {
+    try {
+      if (id) {
+        data?.isFollowing
+          ? await unfolowUser({ userId: id }).unwrap()
+          : await followUser({ followingId: id }).unwrap()
+
+        await triggerGetUserByIdQuery(id)
+
+        await triggerCurrentQuery()
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleClose = async () => {
+    try {
+      if (id) {
+        await triggerGetUserByIdQuery(id)
+        await triggerCurrentQuery()
+        onClose()
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  if (!data) {
+    return null
+  }
+
+  return (
+    <>
+      <GoBack />
+      <div className="flex items-stretch gap-4">
+        <Card className="flex flex-col items-center text-center space-y-4 p-5 flex-2">
+          <Image
+            src={`${BASE_URL}${data.avatarUrl}`}
+            alt={data.name}
+            width={200}
+            height={200}
+            className="border-4 border-white"
+          />
+          <div className="flex flex-col text-2xl font-bold gap-4 items-center">
+            {data.name}
+            {currentUser?.id !== id ? (
+              <Button
+                color={data?.isFollowing ? "default" : "primary"}
+                variant="flat"
+                className="gap-2"
+                onClick={handleFollow}
+                endContent={
+                  data?.isFollowing ? (
+                    <MdOutlinePersonAddDisabled />
+                  ) : (
+                    <MdOutlinePersonAddAlt1 />
+                  )
+                }
+              >
+                {data?.isFollowing ? "Отписаться" : "Подписаться"}
+              </Button>
+            ) : (
+              <Button endContent={<CiEdit />} onClick={() => onOpen()}>
+                Редактировать
+              </Button>
+            )}
+          </div>
+        </Card>
+        <Card className="flex flex-col space-y-4 p-5 flex-1">
+          <ProfileInfo title="Почта:" info={data.email} />
+          <ProfileInfo title="Местоположение:" info={data.location} />
+          <ProfileInfo
+            title="Дата рождения:"
+            info={formatToClientDate(data.dateOfBirth)}
+          />
+          <ProfileInfo title="Обо мне:" info={data.bio} />
+
+          <div className="flex gap-2">
+            <CountInfo count={data.followers.length} title="Подписчики" />
+            <CountInfo count={data.following.length} title="Подписки" />
+          </div>
+        </Card>
+      </div>
+      <EditProfile isOpen={isOpen} onClose={handleClose} user={data} />
+    </>
+  )
 }
